ca055a841f694ead80945f11371b3aa94d21fd89
Kamauleonard <leonardmwangi2012@gmail.com>
2024-01-26 15:18:28 +0300

    Initial Commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        python_list = []
+        the_lists = json.loads(self.get_response_body())
+        for item in the_lists:
+            python_list.append({"name": item['name'], "occupation": item['occupation']})
+
+        return python_list
